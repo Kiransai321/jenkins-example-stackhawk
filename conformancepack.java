f825bd3aaa90123cb6dc6b929f0a05a8694ac6dc
Kiransai321 <94594083+Kiransai321@users.noreply.github.com>
2022-08-10 15:18:59 +0530

    Update conformancepack.java

--- conformancepack.java
+++ conformancepack.java
@@ -3,8 +3,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 // ...
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-@Override
-protected void configure(HttpSecurity http) throws Exception {
+@OverridtpSecurity http) throws Exception {
 http.headers()
 .contentSecurityPolicy("script-src 'self' https://javascripts.example.com; report-uri /csp-report-endpoint/");
 }
